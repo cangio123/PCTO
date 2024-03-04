5db8a1b1253b779e1cfa67bfdb12adab5c349e72
cangio123 <73820424+cangio123@users.noreply.github.com>
2024-03-04 16:00:59 +0000

    Pcto 1

---
+++ esercizio02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    char lettera;
+    lettera='d';
+    
+    printf("%d",lettera);
+return (0);
+}
---
+++ esercizio03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+int a=7;
+int b=13;
+int c=a+b;
+printf("%d",c);
+return(0);
+}
